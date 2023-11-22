058ca0ba1b151d564f62b9b636c6ee8470774df2
RezkitaS <120299952+Rezkitasyarihna@users.noreply.github.com>
2023-11-22 16:36:56 +0700

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -45,6 +45,71 @@ import sys
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 
+# 1ST STEP
+# count each word
+def word_count_dict(filename):
+  # make a dictionary for each number of the word
+  word_count = {}
+  # open a file
+  word_file = open(filename, encoding='utf-8')
+  # we will split the space between each word for each line
+  for line in word_file:
+    words = line.split()
+    # change all the words as lowercase
+    for word in words:
+      word = word.lower()
+      # add the words to the word_count dict
+      if word not in word_count:
+        word_count[word] = 1
+      # add count to the existing word
+      else:
+        word_count[word] += 1
+  return word_count
+
+# 2ND STEP
+# make a print_words() function
+def print_words(filename):
+  result_word_count = word_count_dict(filename)
+  words = sorted(result_word_count)
+  # add loop to print 1 key 1 line
+  for word in words:
+    print(word, result_word_count[word])
+  return
+
+
+
+# 3RD STEP
+# make a tuple for the word count
+
+
+
+# make a print_top(filename) function
+def print_top(filename):
+  # sort the list by the number of each word
+  result_word_count = word_count_dict(filename)
+
+  words = sorted(result_word_count, reverse=True)
+  # result_word_top = sorted_top[:20]
+  
+  for word in words[:20]:
+    print(word, result_word_count[word])
+  # print(sorted_top[:20])
+  return
+
+
+# call the functions
+word_count_dict(r"C:\Users\rzzkt\Downloads\google-python-exercises\google-python-exercises\basic\alice.txt")
+# print_words(r"C:\Users\rzzkt\Downloads\google-python-exercises\google-python-exercises\basic\alice.txt")
+print_top(r"C:\Users\rzzkt\Downloads\google-python-exercises\google-python-exercises\basic\alice.txt")
+
+
+sys.exit(0)
+
+
+
+
+
+
 ###
 
 # This basic command line argument parsing code is provided and
